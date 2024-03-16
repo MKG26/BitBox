a9d125ba73da052684ff7aa930345f28f8e64def
Mohit <92150824+MKG26@users.noreply.github.com>
2024-03-16 20:17:29 +0530

    Finals

---
+++ ObjectDetectBitBox/ObjectDetectBitBox/ContinueView.swift
@@ -0,0 +1,18 @@
+//
+//  ContinueView.swift
+//  ObjectDetectBitBox
+//
+//  Created by Mohit Kumar Gupta on 16/03/24.
+//
+
+import SwiftUI
+
+struct ContinueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContinueView()
+}
